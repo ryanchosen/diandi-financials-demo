d4b3a551856f01eaaeb6c70dea49d74b7ec9668c
RyanSu <563607356@qq.com>
2021-01-07 19:42:50 +0800

    deploy

---
+++ precache-manifest.5b44c864b9ceeed23b9189c722b7a66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3232bcbec75b41db50f7",
+    "url": "/diandi-financials-demo/css/app.7349486f.css"
+  },
+  {
+    "revision": "114875ce4393dcb875f1caec99521e1d",
+    "url": "/diandi-financials-demo/img/qrcode.png"
+  },
+  {
+    "revision": "c63ddfe71b0ade6258bcec8f2c6b0ec1",
+    "url": "/diandi-financials-demo/index.html"
+  },
+  {
+    "revision": "3232bcbec75b41db50f7",
+    "url": "/diandi-financials-demo/js/app.e253eb17.js"
+  },
+  {
+    "revision": "0a4ba42b4ab52a7752db",
+    "url": "/diandi-financials-demo/js/chunk-vendors.85579a6a.js"
+  },
+  {
+    "revision": "bf81b1d4455e442a6660027c53f6c47e",
+    "url": "/diandi-financials-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/diandi-financials-demo/robots.txt"
+  }
+]);
